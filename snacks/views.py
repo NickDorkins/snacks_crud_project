0040c38022d5674553c09bfc45c0a89e2900adf4
NickDorkins <nick.dorkins@gmail.com>
2021-03-08 20:40:57 -0800

    broken code with weird issues

--- snacks/views.py
+++ snacks/views.py
@@ -3,24 +3,25 @@ from .models import Snack
 from django.urls import reverse_lazy
 
 class SnackListView(ListView):
-    template = 'snack_list.html'
+    template = 'snack-list.html'
     model = Snack
 
 class SnackDetailView(DetailView):
-    template = 'snack_detail.html'
+    template = 'snack-detail.html'
     model = Snack
 
 class SnackCreateView(CreateView):
-    template = 'snack_create.html'
+    template = 'snack-create.html'
     model = Snack
     fields = ['title', 'purchaser', 'description']
+    
 
 class SnackUpdateView(UpdateView):
-    template = 'snack_update.html'
+    template = 'snack-update.html'
     model = Snack
     fields = ['title', 'purchaser', 'description']    
 
 class SnackDeleteView(DeleteView):
-    template = 'snack_delete.html'
+    template = 'snack-delete.html'
     model = Snack
     success_url = reverse_lazy('snack_list')
--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ 
-
-# import os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
